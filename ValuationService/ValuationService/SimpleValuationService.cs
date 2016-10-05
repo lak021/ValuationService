62ffb5fd4f06b35a4bb340e14a0451df586a4611
lak021 <vijay.lakshmanan@asx.com.au>
2016-10-05 22:01:03 +1100

    test code

---
+++ ValuationService/ValuationService/SimpleValuationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValuationService
+{
+    public class SimpleValuationService : IValuationService
+    {
+        public double CalculateInstrumentValue(string InstrumentCode, double Price, double Size, string ValuationType)
+        {
+            if (ValuationType == "1")
+                return Price * Size;
+            else return -1.00;
+        }
+
+    }
+}
